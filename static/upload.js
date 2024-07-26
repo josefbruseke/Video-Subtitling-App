f3174ff1ec2e49326b7ba88d77b11377f0659abc
Josef Bruseke <josefbruseke@gmail.com>
2024-07-26 03:17:07 -0300

    add/remove functionalites

--- static/upload.js
+++ static/upload.js
@@ -1,13 +1,15 @@
 document.getElementById('uploadForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
+    document.getElementById('result').innerText = 'The video is being processing! ;)';
+
     let fileInput = document.getElementById('fileInput');
     let file = fileInput.files[0];
 
     let formData = new FormData();
     formData.append('file', file);
-    formData.append('idioma_entrada', document.getElementById('idioma_entrada').value);
     formData.append('idioma_destino', document.getElementById('idioma_destino').value);
+    formData.append('model', document.getElementById('model').value);
 
     fetch('/upload', {
         method: 'POST',
@@ -35,24 +37,34 @@ document.getElementById('uploadForm').addEventListener('submit', function (event
 const languages = [
     "Afrikaans", "Albanian", "Arabic", "Armenian", "Assamese", "Azerbaijani", "Balochi", "Bengali",
     "Burmese", "Cantonese (Yue)", "Cebuano", "Danish", "Dutch", "English", "Estonian", "Filipino",
-    "Flemish", "Galician", "Galician", "German", "Greek", "Hindi", "Hispanic", "Hungarian",
+    "Flemish", "Galician", "German", "Greek", "Hindi", "Hispanic", "Hungarian",
     "Indonesian", "Irish", "Italian", "Japanese", "Kazakh", "Korean", "Lithuanian", "Macedonian",
     "Mandarin (CN)", "Marathi", "Nepali", "Nynorsk", "Pashto", "Polish", "Portuguese", "Romanian",
     "Russian", "Serbian", "Sindhi", "Slovak", "Slovenian", "Spanish", "Swahili", "Tamil", "Thai",
     "Turkish", "Ukrainian", "Urdu", "Vietnamese", "Welsh"
 ];
 
-const idiomaEntradaSelect = document.getElementById('idioma_entrada');
+const models = ["tiny","base", "small", "medium","large"];
+const defaultModel = "medium";
+
+
 const idiomaDestinoSelect = document.getElementById('idioma_destino');
+const modelSelect = document.getElementById('model');
 
 languages.forEach(language => {
-    let optionEntrada = document.createElement('option');
-    optionEntrada.value = language;
-    optionEntrada.textContent = language;
-    idiomaEntradaSelect.appendChild(optionEntrada);
-
     let optionDestino = document.createElement('option');
     optionDestino.value = language;
     optionDestino.textContent = language;
     idiomaDestinoSelect.appendChild(optionDestino);
 });
+
+models.forEach(model => {
+    let optionModel = document.createElement('option');
+    optionModel.value = model;
+    optionModel.textContent = model;
+    if (model === defaultModel) {
+        optionModel.selected = true;
+    }
+    modelSelect.appendChild(optionModel);
+});
+
