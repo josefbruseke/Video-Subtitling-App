f45612a9d139436ce6658e447e6a8dcba795fb03
Josef Bruseke <josefbruseke@gmail.com>
2024-07-29 08:57:00 -0300

    add filename existence check

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+
+
 def format_time(seconds):
     millis = int((seconds - int(seconds)) * 1000)
     seconds = int(seconds)
@@ -6,3 +9,16 @@ def format_time(seconds):
     hours = minutes // 60
     minutes = minutes % 60
     return f"{hours:02}:{minutes:02}:{seconds:02},{millis:03}"
+
+def get_available_filename(base_path, filename):
+    # Extrai o nome do arquivo e a extensão
+    name, ext = os.path.splitext(filename)
+    counter = 0
+    new_filename = filename
+    
+    # Incrementa o sufixo até encontrar um nome de arquivo disponível
+    while os.path.exists(os.path.join(base_path, new_filename)):
+        counter += 1
+        new_filename = f"{name}({counter}){ext}"
+    
+    return new_filename
