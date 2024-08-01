971fbdefd3bbbda53065b5d6daa85cbc1e9d7a66
Josef Bruseke <josefbruseke@gmail.com>
2024-07-31 23:54:49 -0300

    change prompt

--- translate.py
+++ translate.py
@@ -8,18 +8,27 @@ def create_openai_client():
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     return OpenAI(api_key=OPENAI_API_KEY)
 
-def traduzir_texto(texto, idioma_entrada, idioma_destino):
+def define_prompt(idioma_destino, texto):
+    prompt = f"The dictionary below contains the index of a timestamp and the corresponding text. Return a dictionary with the translation of the text to {idioma_destino} with its associated index: {texto}"
+    return prompt
+
+
+def traduzir_texto(texto, idioma_destino):
+    print('Tradução iniciada')
     client = create_openai_client()
-    mensagem = f"Translate this text from {idioma_entrada} to {idioma_destino} without changing its format: {texto}"
-    response = client.chat.completions.create(model="gpt-4o-mini",
+    message = define_prompt(idioma_destino, texto)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "You are a translator."},
-            {"role": "user", "content": mensagem},
+            {"role": "user", "content": message},
         ],
         n=1,
         stop=None,
-        temperature=0.3)
+        temperature=0.2
+    )
     traducao = response.choices[0].message.content.strip()
+    print('Tradução finalizada')
     return traducao
 
 
